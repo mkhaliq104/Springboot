f2dfb1e291b5f9d38f35f677c55a9975128b9d83
Mahnoor Khaliq <mahnoorkhaliq104@gmail.com>
2022-08-07 00:37:00 +0500

    Fixed RestController issue

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController("/users")
+@RestController
 public class UserController {
 
     @GetMapping("/users")
